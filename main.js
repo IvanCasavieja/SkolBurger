3b77ae7496733814b3fffee69cb3bb67f12057b1
Ivan_Casavieja <ivanmichelcasavieja@gmail.com>
2024-01-11 10:04:10 -0300

    Operaciones

--- main.js
+++ main.js
@@ -19,17 +19,32 @@ if (conf === "si") {
   alert("Aguarde en linea mientras lo comunicamos con el area.");
   for (let i = 3; i > 0; i--) {
     alert("En " + i);
-  } 
+  }
+  alert("Esta parte no tiene nada que ver, pero como decia que habia que poner operaciones como en clase tuve que inventar algo");
+  let palabraUno = prompt("Ingrese una palabra");
+  if (palabraUno === "" || palabraUno === null) {
+    palabraUno = bucleInfinito(palabraUno);
+  }
+  let palabraDos = prompt("Ingrese optra palabra");
+  if (palabraDos === "" || palabraDos === null) {
+    palabraDos = bucleInfinito(palabraDos);
+  }
+  let resultado = sumaX(palabraUno, palabraDos)
+  alert("La junta de estas dos palabras es " + resultado);
 } else if (conf === "no") {
   alert("Ah, que macana pense que ya habia conseguido laburo");
 } else {
   alert('Proba con "si" o "no", el resto de respuestas es mucho para mi');
 }
 
-/* Function buicle infinito */
+/* Function bucle infinito */
 function bucleInfinito(datoUno) {
   while (datoUno === "" || datoUno === null) {
     datoUno = prompt("Ingrese un dato valido");
   }
   return datoUno;
+}
+
+function sumaX(datoUno, datoDos) {
+  return datoUno + " " + datoDos;
 }
