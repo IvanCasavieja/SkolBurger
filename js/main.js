1ea70a3e9f435fbde2e9268435608aaf1a96c80f
Ivan_Casavieja <ivanmichelcasavieja@gmail.com>
2024-02-07 22:53:10 -0300

    correccion de contenedores

--- js/main.js
+++ js/main.js
@@ -148,37 +148,6 @@ document.addEventListener("DOMContentLoaded", function () {
   function cerrarContenedorIngredientes() {
     contenedorSinIngredientes.style.display = "none";
   }
-  function mostrarContenedor(contenedorId, elementoLista) {
-    ocultarContenedores();
-
-    /* Quitar la clase 'seleccionado' de todos los elementos de la lista */
-    let elementosLista = document.querySelectorAll("#listaProductos li");
-    elementosLista.forEach(function (elemento) {
-      elemento.classList.remove("seleccionado");
-    });
-
-    /*  Mostrar el contenedor */
-    let contenedor = document.getElementById(contenedorId);
-    if (contenedor) {
-      contenedor.style.display = "flex";
-    }
-
-    /*   Agregar la clase 'seleccionado' al elemento */
-    elementoLista.classList.add("seleccionado");
-  }
-
-  /* Función para ocultar todos los contenedores */
-  function ocultarContenedores() {
-    let contenedores = document.getElementsByClassName("contenedor");
-    for (let i = 0; i < contenedores.length; i++) {
-      contenedores[i].style.display = "none";
-    }
-  }
-
-  /* Mostrar el contenedor de Combos al inicio */
-  mostrarContenedor("contenedorCombos", document.getElementById("combos"));
-
-  /* --------------------------------------------------------------------------- */
   let horariosAbierto = false;
   let horariosContenedor = document.querySelector("#horariosContenedor");
 
@@ -205,3 +174,35 @@ document.addEventListener("DOMContentLoaded", function () {
     }
   }
 });
+
+function mostrarContenedor(contenedorId, elementoLista) {
+  ocultarContenedores();
+
+  /* Quitar la clase 'seleccionado' de todos los elementos de la lista */
+  let elementosLista = document.querySelectorAll("#listaProductos li");
+  elementosLista.forEach(function (elemento) {
+    elemento.classList.remove("seleccionado");
+  });
+
+  /*  Mostrar el contenedor */
+  let contenedor = document.getElementById(contenedorId);
+  if (contenedor) {
+    contenedor.style.display = "flex";
+  }
+
+  /*   Agregar la clase 'seleccionado' al elemento */
+  elementoLista.classList.add("seleccionado");
+}
+
+/* Función para ocultar todos los contenedores */
+function ocultarContenedores() {
+  let contenedores = document.getElementsByClassName("contenedor");
+  for (let i = 0; i < contenedores.length; i++) {
+    contenedores[i].style.display = "none";
+  }
+}
+
+/* Mostrar el contenedor de Combos al inicio */
+mostrarContenedor("contenedorCombos", document.getElementById("combos"));
+
+/* --------------------------------------------------------------------------- */
